2c5acca90f9c839956dfa9b84140bb2e13ab7d07
NhOuOiB <s33165309000@gmail.com>
2022-08-14 12:39:19 +0800

    update crawler3.js install axios

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -4,32 +4,25 @@ const axios = require('axios');
 const moment = require('moment');
 const fsPromises = require('fs/promises');
 
+let queryDate = moment().format('YYYYMMDD');
 async function read() {
   try {
-    let promise = fsPromises.readFile('stockNo.txt', 'utf8');
     // console.log(await promise);
-    let stockNo = await promise;
-    (async () => {
-      let response = await axios
-        .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?', {
-          params: {
-            response: 'json',
-            date: queryDate,
-            stockNo: stockNo,
-          },
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      console.log(response.data);
-    })();
+    let stockNo = await fsPromises.readFile('stockNo.txt', 'utf8');
+    let response = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?', {
+      params: {
+        response: 'json',
+        date: queryDate,
+        stockNo: stockNo,
+      },
+    });
+    console.log(response.data);
   } catch (err) {
     console.error(err);
   }
 }
-read();
 
-let queryDate = moment().format('YYYYMMDD');
+read();
 
 // (async () => {
 //   let response = await axios
