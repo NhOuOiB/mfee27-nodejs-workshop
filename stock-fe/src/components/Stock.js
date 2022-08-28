64e822d00bc7f3899b38699c14370ca7b9fbcc4e
Mike <s33165309000@gmail.com>
2022-08-28 13:55:51 +0800

    [feat]pagination

--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -7,11 +7,9 @@ const Stock = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    console.log('Stock', 'useEffect []');
-    console.log('useEffect[]', data);
     let getStock = async () => {
       let response = await axios.get('http://localhost:3001/api/stocks');
-      console.log(response.data);
+      // console.log(response.data);
 
       setData(response.data);
     };
@@ -23,6 +21,7 @@ const Stock = () => {
     <div>
       {error && <div>{error}</div>}
       <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
+<<<<<<< HEAD
 
       <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
         <Link to={`/stock/1234`}>
@@ -30,6 +29,18 @@ const Stock = () => {
           <p className="text-gray-700">1234名稱</p>
         </Link>
       </div>
+=======
+      {data.map((stock) => {
+        return (
+          <div key={stock.id} className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/${stock.id}`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
+>>>>>>> 3722772 ([feat]pagination)
     </div>
   );
 };
